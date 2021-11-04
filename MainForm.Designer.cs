639540cbf03519d1a6a7c499f079a9947dd6f9cd
Andrew Vaimon <niko142000@mail.ru>
2021-11-04 23:57:38 +0300

    big parsing improvement

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -40,10 +40,16 @@ namespace ProductionSystem
             this.checkListPrice = new System.Windows.Forms.CheckedListBox();
             this.groupBox4 = new System.Windows.Forms.GroupBox();
             this.checkListLocation = new System.Windows.Forms.CheckedListBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.checkListDrinks = new System.Windows.Forms.CheckedListBox();
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.checkListCompany = new System.Windows.Forms.CheckedListBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
             this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.groupBox6.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -51,9 +57,9 @@ namespace ProductionSystem
             this.groupBox1.BackColor = System.Drawing.SystemColors.Control;
             this.groupBox1.Controls.Add(this.textOutput);
             this.groupBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.groupBox1.Location = new System.Drawing.Point(0, 608);
+            this.groupBox1.Location = new System.Drawing.Point(0, 546);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(978, 336);
+            this.groupBox1.Size = new System.Drawing.Size(995, 398);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Вывод";
@@ -69,7 +75,7 @@ namespace ProductionSystem
             this.textOutput.Name = "textOutput";
             this.textOutput.ReadOnly = true;
             this.textOutput.ShortcutsEnabled = false;
-            this.textOutput.Size = new System.Drawing.Size(972, 306);
+            this.textOutput.Size = new System.Drawing.Size(989, 368);
             this.textOutput.TabIndex = 0;
             this.textOutput.TabStop = false;
             this.textOutput.Text = "";
@@ -77,9 +83,10 @@ namespace ProductionSystem
             // btnStart
             // 
             this.btnStart.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnStart.Location = new System.Drawing.Point(635, 411);
+            this.btnStart.Enabled = false;
+            this.btnStart.Location = new System.Drawing.Point(741, 497);
             this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(242, 58);
+            this.btnStart.Size = new System.Drawing.Size(242, 43);
             this.btnStart.TabIndex = 1;
             this.btnStart.Text = "Запустить вывод";
             this.btnStart.UseVisualStyleBackColor = true;
@@ -87,12 +94,13 @@ namespace ProductionSystem
             // 
             // checkListFeatures
             // 
-            this.checkListFeatures.CheckOnClick = true;
+            this.checkListFeatures.ColumnWidth = 350;
             this.checkListFeatures.Dock = System.Windows.Forms.DockStyle.Fill;
             this.checkListFeatures.FormattingEnabled = true;
             this.checkListFeatures.Location = new System.Drawing.Point(3, 27);
+            this.checkListFeatures.MultiColumn = true;
             this.checkListFeatures.Name = "checkListFeatures";
-            this.checkListFeatures.Size = new System.Drawing.Size(475, 404);
+            this.checkListFeatures.Size = new System.Drawing.Size(965, 137);
             this.checkListFeatures.TabIndex = 2;
             this.checkListFeatures.ThreeDCheckBoxes = true;
             // 
@@ -100,7 +108,7 @@ namespace ProductionSystem
             // 
             this.rbForward.AutoSize = true;
             this.rbForward.Checked = true;
-            this.rbForward.Location = new System.Drawing.Point(694, 323);
+            this.rbForward.Location = new System.Drawing.Point(505, 504);
             this.rbForward.Name = "rbForward";
             this.rbForward.Size = new System.Drawing.Size(104, 29);
             this.rbForward.TabIndex = 4;
@@ -111,7 +119,7 @@ namespace ProductionSystem
             // rbBackward
             // 
             this.rbBackward.AutoSize = true;
-            this.rbBackward.Location = new System.Drawing.Point(694, 358);
+            this.rbBackward.Location = new System.Drawing.Point(615, 504);
             this.rbBackward.Name = "rbBackward";
             this.rbBackward.Size = new System.Drawing.Size(121, 29);
             this.rbBackward.TabIndex = 4;
@@ -121,9 +129,9 @@ namespace ProductionSystem
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.checkListFeatures);
-            this.groupBox2.Location = new System.Drawing.Point(12, 168);
+            this.groupBox2.Location = new System.Drawing.Point(12, 324);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(481, 434);
+            this.groupBox2.Size = new System.Drawing.Size(971, 167);
             this.groupBox2.TabIndex = 5;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Фишечки";
@@ -131,9 +139,9 @@ namespace ProductionSystem
             // groupBox3
             // 
             this.groupBox3.Controls.Add(this.checkListPrice);
-            this.groupBox3.Location = new System.Drawing.Point(499, 12);
+            this.groupBox3.Location = new System.Drawing.Point(502, 12);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(467, 150);
+            this.groupBox3.Size = new System.Drawing.Size(481, 150);
             this.groupBox3.TabIndex = 5;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Бюджет";
@@ -145,9 +153,10 @@ namespace ProductionSystem
             this.checkListPrice.FormattingEnabled = true;
             this.checkListPrice.Location = new System.Drawing.Point(3, 27);
             this.checkListPrice.Name = "checkListPrice";
-            this.checkListPrice.Size = new System.Drawing.Size(461, 120);
+            this.checkListPrice.Size = new System.Drawing.Size(475, 120);
             this.checkListPrice.TabIndex = 2;
             this.checkListPrice.ThreeDCheckBoxes = true;
+            this.checkListPrice.SelectedIndexChanged += new System.EventHandler(this.checkListPrice_SelectedIndexChanged);
             // 
             // groupBox4
             // 
@@ -169,13 +178,60 @@ namespace ProductionSystem
             this.checkListLocation.Size = new System.Drawing.Size(475, 120);
             this.checkListLocation.TabIndex = 2;
             this.checkListLocation.ThreeDCheckBoxes = true;
+            this.checkListLocation.SelectedIndexChanged += new System.EventHandler(this.checkListLocation_SelectedIndexChanged);
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.checkListDrinks);
+            this.groupBox5.Location = new System.Drawing.Point(12, 168);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(481, 150);
+            this.groupBox5.TabIndex = 5;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Вид напитков";
+            // 
+            // checkListDrinks
+            // 
+            this.checkListDrinks.CheckOnClick = true;
+            this.checkListDrinks.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.checkListDrinks.FormattingEnabled = true;
+            this.checkListDrinks.Location = new System.Drawing.Point(3, 27);
+            this.checkListDrinks.Name = "checkListDrinks";
+            this.checkListDrinks.Size = new System.Drawing.Size(475, 120);
+            this.checkListDrinks.TabIndex = 2;
+            this.checkListDrinks.ThreeDCheckBoxes = true;
+            this.checkListDrinks.SelectedIndexChanged += new System.EventHandler(this.checkListDrinks_SelectedIndexChanged);
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.Controls.Add(this.checkListCompany);
+            this.groupBox6.Location = new System.Drawing.Point(502, 168);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Size = new System.Drawing.Size(481, 150);
+            this.groupBox6.TabIndex = 5;
+            this.groupBox6.TabStop = false;
+            this.groupBox6.Text = "Размер компании";
+            // 
+            // checkListCompany
+            // 
+            this.checkListCompany.CheckOnClick = true;
+            this.checkListCompany.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.checkListCompany.FormattingEnabled = true;
+            this.checkListCompany.Location = new System.Drawing.Point(3, 27);
+            this.checkListCompany.Name = "checkListCompany";
+            this.checkListCompany.Size = new System.Drawing.Size(475, 120);
+            this.checkListCompany.TabIndex = 2;
+            this.checkListCompany.ThreeDCheckBoxes = true;
+            this.checkListCompany.SelectedIndexChanged += new System.EventHandler(this.checkListCompany_SelectedIndexChanged);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(978, 944);
+            this.ClientSize = new System.Drawing.Size(995, 944);
+            this.Controls.Add(this.groupBox5);
             this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox6);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.rbBackward);
@@ -191,6 +247,8 @@ namespace ProductionSystem
             this.groupBox2.ResumeLayout(false);
             this.groupBox3.ResumeLayout(false);
             this.groupBox4.ResumeLayout(false);
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox6.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -209,6 +267,10 @@ namespace ProductionSystem
         private System.Windows.Forms.CheckedListBox checkListPrice;
         private System.Windows.Forms.GroupBox groupBox4;
         private System.Windows.Forms.CheckedListBox checkListLocation;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.CheckedListBox checkListDrinks;
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.CheckedListBox checkListCompany;
     }
 }
 
--- MainForm.cs
+++ MainForm.cs
@@ -11,6 +11,8 @@ using System.Windows.Forms;
 
 namespace ProductionSystem
 {
+    public enum InitialFactType { DRINK_TYPE, BUDGET, LOCATION, COMPANY_SIZE, FEATURE, COMMON, OPPOSITE_FEATURE };
+
     public partial class MainForm : Form
     {
         const String dbFileName = "..\\..\\..\\db\\db.txt";
@@ -24,9 +26,26 @@ namespace ProductionSystem
             rules = new Dictionary<int, Rule>();
             loadDB();
 
-            checkListFeatures.Items.AddRange(facts.Where(x => x.Value.isInitial && !x.Value.isPrice && !x.Value.isLocation).Select(x=> new FactWrapper(x)).ToArray());
-            checkListPrice.Items.AddRange(facts.Where(x => x.Value.isInitial && x.Value.isPrice).Select(x => new FactWrapper(x)).ToArray());
-            checkListLocation.Items.AddRange(facts.Where(x => x.Value.isInitial && x.Value.isLocation).Select(x => new FactWrapper(x)).ToArray());
+            checkListFeatures.Items.AddRange(facts.Where(x => x.Value is InitialFact && (x.Value as InitialFact).factType == InitialFactType.FEATURE).Select(x=> new FactWrapper(x)).ToArray());
+            checkListPrice.Items.AddRange(facts.Where(x => x.Value is InitialFact && (x.Value as InitialFact).factType == InitialFactType.BUDGET).Select(x => new FactWrapper(x)).ToArray());
+            checkListLocation.Items.AddRange(facts.Where(x => x.Value is InitialFact && (x.Value as InitialFact).factType == InitialFactType.LOCATION).Select(x => new FactWrapper(x)).ToArray());
+            checkListCompany.Items.AddRange(facts.Where(x => x.Value is InitialFact && (x.Value as InitialFact).factType == InitialFactType.COMPANY_SIZE).Select(x => new FactWrapper(x)).ToArray());
+            checkListDrinks.Items.AddRange(facts.Where(x => x.Value is InitialFact && (x.Value as InitialFact).factType == InitialFactType.DRINK_TYPE).Select(x => new FactWrapper(x)).ToArray());
+
+        }
+
+        InitialFactType parseType(String type)
+        {
+            switch (type)
+            {
+                case "fia": return InitialFactType.DRINK_TYPE;
+                case "fib": return InitialFactType.BUDGET;
+                case "fic": return InitialFactType.COMPANY_SIZE;
+                case "fil": return InitialFactType.LOCATION;
+                case "fi": return InitialFactType.FEATURE;
+                case "fn": return InitialFactType.OPPOSITE_FEATURE;
+                default: throw new Exception("Типы фактов всё сломали :(");
+            }
         }
 
         void loadDB()
@@ -37,13 +56,28 @@ namespace ProductionSystem
                 {
                     var data = sr.ReadLine().Split(';');
                     var id = data[0].Split('-');
-                    if (id[0].StartsWith("f"))
+                    if (id[0].Equals("f"))
                     {
-                        var text = data[1];
-                        bool isPriceFact = text.Contains("Посидеть на");
-                        bool isLocationFact = text.Contains("Располагается");
-                        facts.Add(int.Parse(id[1]), new Fact(data[1],isFinite: id[0].Equals("ff"), isInitial: id[0].Equals("fi"), isPrice: isPriceFact, isLocation:isLocationFact));
-                    }else if (data[0].StartsWith("r"))
+                        facts.Add(int.Parse(id[1]), new Fact(data[1]));
+                    }
+                    else if (id[0].StartsWith("f"))
+                    {
+                        if (id[0].Equals("ff"))
+                        {
+                            facts.Add(int.Parse(id[1]), new FiniteFact(data[1]));
+                            continue;
+                        }
+                        var fact = new InitialFact(data[1], parseType(id[0]));
+                        if(fact.factType == InitialFactType.OPPOSITE_FEATURE)
+                        {
+                            var ids = id[1].Split('/');
+                            (facts[int.Parse(ids[1])] as InitialFact).oppositeFact = int.Parse(ids[0]);
+                            facts.Add(int.Parse(ids[0]),fact);
+                            continue;
+                        }
+                        facts.Add(int.Parse(id[1]), fact);
+                    }
+                    else if (data[0].StartsWith("r"))
                     {
                         var premises = data[1].Split(',').Select(x => int.Parse(x.Split('-')[1])).ToList();
                         rules.Add(int.Parse(id[1]), new Rule(premises,int.Parse(data[2].Split('-')[1]),data[3]));
@@ -71,6 +105,36 @@ namespace ProductionSystem
         {
 
         }
+
+        bool areAllListsContainsCheckedItems()
+        {
+            return (checkListCompany.CheckedIndices.Count > 0) && (checkListDrinks.CheckedIndices.Count > 0) && (checkListLocation.CheckedIndices.Count > 0) && (checkListPrice.CheckedIndices.Count > 0);
+        }
+
+        private void checkListLocation_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            btnStart.Enabled = areAllListsContainsCheckedItems();
+        }
+
+        private void checkListLocation_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            btnStart.Enabled = areAllListsContainsCheckedItems();
+        }
+
+        private void checkListPrice_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            btnStart.Enabled = areAllListsContainsCheckedItems();
+        }
+
+        private void checkListDrinks_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            btnStart.Enabled = areAllListsContainsCheckedItems();
+        }
+
+        private void checkListCompany_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            btnStart.Enabled = areAllListsContainsCheckedItems();
+        }
     }
 
     public class Rule
@@ -90,18 +154,30 @@ namespace ProductionSystem
     public class Fact
     {
         public String fact;
-        public bool isFinite;
-        public bool isInitial;
-        public bool isPrice;
-        public bool isLocation;
 
-        public Fact(String fact, bool isFinite = false, bool isInitial = false, bool isPrice = false, bool isLocation = false)
+        public Fact(String fact)
         {
             this.fact = fact;
-            this.isFinite = isFinite;
-            this.isInitial = isInitial;
-            this.isPrice = isPrice;
-            this.isLocation = isLocation;
+        }
+    }
+
+    public class InitialFact : Fact
+    {
+        public InitialFactType factType;
+        public int oppositeFact;
+
+        public InitialFact(String fact, InitialFactType type, int oppositeFact = -1) : base(fact)
+        {
+            this.factType = type;
+            this.oppositeFact = oppositeFact;
+        }
+    }
+
+    public class FiniteFact: Fact
+    {
+        public FiniteFact(String fact) : base(fact)
+        {
+
         }
     }
 
